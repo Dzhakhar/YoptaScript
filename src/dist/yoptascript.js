66c4c2137ef06cdcc6f56df1b76a7290b61d0920
dzhakhar <dzhakhar.ukhaev@gmail.com>
2016-12-09 00:20:26 +0600

    разделение npm и браузерной версии

--- src/dist/yoptascript.js
+++ src/dist/yoptascript.js
@@ -49,7 +49,7 @@
 
 /***/ },
 /* 1 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	"use strict";
 
@@ -57,24 +57,72 @@
 	  value: true
 	});
 
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /*!
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * YoptaScript v0.2.6 (https://yopta.space)
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * Copyright (c) 2016 Yopta.Space project and Contributors
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * Licensed under the MIT license
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      */
+
+
+	var _words = __webpack_require__(3);
+
+	var _words2 = _interopRequireDefault(_words);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 	var Yopta = function () {
-	  function Yopta(props) {
+	  function Yopta() {
 	    _classCallCheck(this, Yopta);
 	  }
 
 	  _createClass(Yopta, [{
 	    key: "compile",
-	    value: function compile() {}
+	    value: function compile(yoptaCode) {
+	      return yoptaCode.replace(/([а-яА-Я]+)/g, function (e) {
+	        if (_words2.default[e]) {
+	          return _words2.default[e];
+	        } else {
+	          return e;
+	        }
+	      });
+	    }
 	  }]);
 
 	  return Yopta;
 	}();
 
+	var yopta = new Yopta();
+
+	if (window) {
+	  document.querySelectorAll('[language="YoptaScript"]').forEach(yopta.compile);
+	}
+
 	exports.default = Yopta;
+
+/***/ },
+/* 2 */,
+/* 3 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	/*!
+	 * YoptaScript v0.2.6 (https://yopta.space)
+	 * Copyright (c) 2016 Yopta.Space project and Contributors
+	 * Licensed under the MIT license
+	 */
+
+	// Меняем массив на объект key:value
+	var _WORDS_ = {
+	  "эквалио": "=="
+	};
+
+	exports.default = _WORDS_;
 
 /***/ }
 /******/ ]);
--- src/lib/yoptascript.jsx
+++ src/lib/yoptascript.jsx
@@ -1,10 +1,26 @@
+/*!
+ * YoptaScript v0.2.6 (https://yopta.space)
+ * Copyright (c) 2016 Yopta.Space project and Contributors
+ * Licensed under the MIT license
+ */
+import words from "./words.jsx";
+
 class Yopta {
-  constructor(props){
+  compile(yoptaCode){
+    return yoptaCode.replace(/([а-яА-Я]+)/g, function(e){
+      if(words[e]){
+        return words[e];
+      } else {
+        return e;
+      }
+    })
   }
+}
 
-  compile(){
+const yopta = new Yopta();
 
-  }
+if(window){
+  document.querySelectorAll('[language="YoptaScript"]').forEach(yopta.compile);
 }
 
 export default Yopta;
